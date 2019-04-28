10b00611b1f92a9dba7092ccf44bf72f33300b10
Murray <murray@mbp.local>
2019-04-28 22:34:23 +0800

    add server

---
+++ build/webpack.config.server.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const webpack = require('webpack')
+// const VueLoaderPlugin = require('vue-loader/lib/plugin')
+// const CleanWebpackPlugin = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+
+let config
+
+config = merge(baseConfig, {
+  target: 'node', // 定义打包出的js所运行的环境
+  mode: 'development',
+  devtool: 'source-map',
+  entry: path.join(__dirname, '../client/server-entry.js'),
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../server-build')
+  },
+  // 排除要打包的文件，直接从node_modules里面引用即可，防止引用两次
+  externals: Object.keys(require('../package.json').dependencies),
+  resolve: {
+    alias: {
+      'vue': path.join(__dirname, '../node_modules/vue/dist/vue.esm.js')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contentHash:8].css'
+      // chunkFilename: '[id].css'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.VUE_ENV': '"server"'
+    }),
+    new VueServerPlugin()
+  ]
+})
+
+module.exports = config
---
+++ server/routers/dev-ssr.js
@@ -0,0 +1,19 @@
+const Router = require('koa-router')
+const axios = require('axios')
+const MemoryFS = require('memory-fs') //处理内存中文件
+const webpack = require('webpack')
+const VueServerRenderer = require('vue-server-renderer')
+
+const serverConfig = require('../../build/webpack.config.server')
+
+const serverCompiler = webpack(serverConfig)
+const mfs = new MemoryFS()
+serverCompiler.outputFileSystem = mfs
+
+let bundle
+serverCompiler.watch({}, (err, stats) => {
+  if (err) throw err
+  stats = stats.toJson()
+  stats.errors.forEach(err => console.log(err))
+  stats.warnings.forEach(warn => console.warn(err))
+})
